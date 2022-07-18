5a90d69be4c1979e5dd495747d2a33f23a8b2cf0
Benhakim Josue <benjosue@icloud.com>
2022-07-18 16:02:43 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,66 @@
+import requests
+import json 
+import sqlite3
+
+conn = sqlite3.connect('hotels.db')
+c = conn.cursor()
+
+def find_hotel():
+    
+    url = "https://hotel-price-aggregator.p.rapidapi.com/search"
+
+    city_input = input("Enter city of desired stay:    ")
+    querystring = {"q":city_input}
+    print("    ")
+    print("    ")
+    headers = {
+	"X-RapidAPI-Key": "0ceacf4ba5msh85fc64cdf73f882p1a35fdjsn5c1ff6ca4a33",
+	"X-RapidAPI-Host": "hotel-price-aggregator.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()            
+ 
+    return data
+
+def get_hotel_rate():
+
+    selected_hotelId = input("Enter the hoteId that correlates to your desired location:    ")
+    checkIn =  input("Enter your desired check in date in the following format, year-month-day (XXXX-XX-XX):     ")
+    checkOut = input("Enter your desired check out date in the following format, year-month-day (XXXX-XX-XX):    ")
+    
+    url = "https://hotel-price-aggregator.p.rapidapi.com/rates"
+    api_key = "0ceacf4ba5msh85fc64cdf73f882p1a35fdjsn5c1ff6ca4a33"
+    
+
+    payload =    {
+    "hotelId": selected_hotelId,
+    "checkIn": checkIn,
+    "checkOut": checkOut
+    }
+
+    headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": api_key,
+    "X-RapidAPI-Host": "hotel-price-aggregator.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    hotel_stats = response.json()
+
+    for key in hotel_stats:
+        if key == 'rates':
+            return hotel_stats[key]
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS hotelrates(provider TEXT, host TEXT, rate REAL, tax REAL)") 
+
+def data_entry():
+    c.execute("INSERT INTO hotelrates (provider, host, rate, tax) VALUES (?, ?, ?, ?)"), (provider, host, rate, tax)
+    conn.commit()
+    c.close()
+    con.close()
+
+    
+ 
